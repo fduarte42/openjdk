6f2598e217226c5abefecc626ce9274efd4c0495
Tito Duarte <t.duarte@orangehive.de>
2016-11-07 11:47:07 +0100

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-exec ant run
+ant run 1>/tmp/output.log 2>&1 && \
+    echo /tmp/output.log || \
+    while true; do cat /tmp/output.log; { echo -ne "HTTP/1.0 200 OK\r\n\r\n"; cat /tmp/output.log; } | nc -l -p 80; test $? -gt 128 && break; done
